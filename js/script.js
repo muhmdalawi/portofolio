4b10a7f052ba207d65489c34f36d93e4d4bb9641
Muhamad Alawi <alawi@MacBook-Air-Muhamad.local>
2025-06-13 09:11:55 +0700

    Upload semua file portfolio termasuk folder

---
+++ js/script.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("contactForm");
+    const scriptURL = "https://script.google.com/macros/s/AKfycbyNP1VpqvtxekzCVFmGXu6HS8SEdTC0sJMC3OjJmWkuksLw1HAcgH1S-zv6uPgpv118/exec";
+  
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+    
+        fetch(scriptURL, {
+          method: "POST",
+          body: new FormData(form)
+        })
+        .then(() => {
+          alert("Terima kasih! Pesan Anda telah dikirim.");
+          form.reset();
+        })
+        .catch((error) => {
+          console.error("Error!", error.message);
+          alert("Terjadi kesalahan. Coba lagi nanti.");
+        });
+      });
+  });
+  
+
+  function openModal(imageSrc, description) {
+    document.getElementById('modalImage').src = imageSrc;
+    document.getElementById('modalDesc').textContent = description;
+    document.getElementById('modal').style.display = 'block';
+  }
+  
+  function closeModal() {
+    document.getElementById('modal').style.display = 'none';
+  }
+  
+  window.onclick = function(event) {
+    const modal = document.getElementById('modal');
+    if (event.target == modal) {
+      modal.style.display = 'none';
+    }
+  }
+
+  function toggleMenu() {
+    document.querySelector('.nav-links').classList.toggle('active');
+  }
+  
+  
