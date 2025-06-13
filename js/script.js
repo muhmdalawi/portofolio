50688de5b0c69b1f24a6ed580afdf2669d7fc519
muhmdalawi <muhmdalawi05@gmail.com>
2025-06-13 09:45:53 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,24 +1,35 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const form = document.getElementById("contactForm");
-    const scriptURL = "https://script.google.com/macros/s/AKfycbyDyzNNasAx_1jUjZIc7i0IJjNMtGiPU94O88FZYqMZlwdpoBX8DaI1rdv_ABviyCCi/exec";
-  
-    form.addEventListener("submit", function (e) {
-        e.preventDefault();
-    
-        fetch(scriptURL, {
-          method: "POST",
-          body: new FormData(form)
-        })
-        .then(() => {
-          alert("Terima kasih! Pesan Anda telah dikirim.");
-          form.reset();
-        })
-        .catch((error) => {
-          console.error("Error!", error.message);
-          alert("Terjadi kesalahan. Coba lagi nanti.");
-        });
-      });
-  });
+const scriptURL =
+                'https://script.google.com/macros/s/AKfycbyZPuKhiwX_JBCvzwOsYYX3RDq_WraRCego-h8HEncwVafnWn5ELmaaFS8WxxAXsfl9uQ/exec'
+            const form = document.forms['submit-to-google-sheet'];
+            const btnSubmit = document.querySelector('.btn-submit');
+            const btnReset = document.querySelector('.btn-reset');
+            const btnLoading = document.querySelector('.btn-loading');
+            const MyAlert = document.querySelector('.my-alert');
+
+            form.addEventListener('submit', e => {
+                e.preventDefault()
+
+                //ketika tombol submit diklik
+
+                btnLoading.classList.toggle('d-none');
+                btnSubmit.classList.toggle('d-none');
+                btnReset.classList.toggle('d-none');
+
+                fetch(scriptURL, {
+                        method: 'POST',
+                        body: new FormData(form)
+                    })
+                    .then(response => {
+                        btnLoading.classList.toggle('d-none');
+                        btnSubmit.classList.toggle('d-none');
+                        btnReset.classList.toggle('d-none');
+                        MyAlert.classList.toggle('d-none');
+                        //RESET
+                        form.reset();
+                        console.log('Success!', response)
+                    })
+                    .catch(error => console.error('Error!', error.message))
+            })
   
 
   function openModal(imageSrc, description) {
